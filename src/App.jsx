f689715eb51fcf7b38fb5ac046d5c6b48c7a03a6
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-08-07 11:34:41 +0700

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from 'react'
+import NavBar from './components/NavBar.jsx'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home  from './pages/Home.jsx'
+import Movies from './pages/Movies.jsx'
+import SeatLayout from './pages/SeatLayout.jsx'
+import MyBookings from './pages/MyBookings.jsx'
+import Favorite from './pages/Favorite.jsx'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default App;
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+    <>
+      <Toaster/>
+      { !isAdminRoute && <NavBar/>}
+      <Routes>
+        <Route  path={'/'} element = {<Home/>}/>
+        <Route  path={'/movies'} element = {<Movies/>}/>
+        <Route  path={'/movies:id'} element = {<SeatLayout/>}/>
+        <Route  path={'/my-bookings'} element = {<MyBookings/>}/>
+        <Route  path={'/favorite'} element = {<Favorite/>}/>
+      </Routes>
+      { !isAdminRoute && <Footer/>}
+    </>
+  )
+}
+
+export default App
