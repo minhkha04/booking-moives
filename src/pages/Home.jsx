42c9e3601f93d6563ec4bf5acda816727b57561b
Le Nguyen Minh Kha <lenguyenminhkha1606@gmail.com>
2025-08-07 17:39:00 +0700

    add TrailersSection component and integrate into Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection.jsx'
 import FeaturedSection from '../components/FeaturedSection.jsx'
+import TrailersSection from '../components/TrailersSection.jsx'
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
       <FeaturedSection/>
+      <TrailersSection/>
     </>
   )
 }
